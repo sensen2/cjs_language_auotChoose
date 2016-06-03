b6bea0b1c5e10241ba5a9781602bf9c7a74d4d0e
sensen2 <1464353651@qq.com>
2016-06-03 17:14:23 +0800

    第一次修改代码

--- app/src/main/java/com/qianfeng/cjs/myapplicat/MainActivity.java
+++ app/src/main/java/com/qianfeng/cjs/myapplicat/MainActivity.java
@@ -2,6 +2,7 @@ package com.qianfeng.cjs.myapplicat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -9,5 +10,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.tv);
+        tv.setText("第一次修改了代码");
     }
 }
